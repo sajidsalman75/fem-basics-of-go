a4a6e587308d5b6e48ba34fc7965c3d7fc2b88f8
Salman Sajid <sajidsalman75@gmail.com>
2024-04-05 21:47:48 +0500

    wait group and response struct

--- cryptomasters/api/cex.go
+++ cryptomasters/api/cex.go
@@ -16,19 +16,20 @@ func GetRate(currency string) (*datatypes.Rate, error) {
 	if err != nil {
 		return nil, err
 	}
+	var response CEXResponse
 	if res.StatusCode == http.StatusOK {
 		bodyBytes, err := io.ReadAll(res.Body)
 		if err != nil {
 			return nil, err
 		}
-		var cryptoRate datatypes.Rate
-		err = json.Unmarshal(bodyBytes, &cryptoRate)
+
+		err = json.Unmarshal(bodyBytes, &response)
 		if err != nil {
 			return nil, err
 		}
 	} else {
 		return nil, fmt.Errorf("status code received: %v", res.StatusCode)
 	}
-	rate := datatypes.Rate{Currency: currency, Price: 20}
+	rate := datatypes.Rate{Currency: currency, Price: response.Bid}
 	return &rate, nil
 }
--- cryptomasters/main.go
+++ cryptomasters/main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fem/go/cryptomasters/api"
+	"fmt"
+	"sync"
 )
 
 func main() {
-	rate, err := api.GetRate("BTC")
-	print(rate.Price, err)
+	currencies := []string{"BTC", "ETH", "BCH"}
+	var wg sync.WaitGroup
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func(currencyCode string) {
+			getCurrencyData(currencyCode)
+			wg.Done()
+		}(currency)
+	}
+	wg.Wait()
+}
+
+func getCurrencyData(currency string) {
+	rate, err := api.GetRate(currency)
+	if err == nil {
+		fmt.Printf("The rate for %v is %.2f\n", rate.Currency, rate.Price)
+	}
 }
